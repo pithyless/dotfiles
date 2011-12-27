0874e90d8274dc06cbcd217a0af0e54b6fa8c614
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-12-27 15:50:33 +0100

    add linux alias: mongo

--- mongo/aliases.zsh
+++ mongo/aliases.zsh
@@ -1,6 +1,7 @@
 if [ ${OSTYPE:0:5} = "linux" ]; then
     alias mongo-up="sudo /etc/init.d/mongodb start"
     alias mongo-down="sudo /etc/init.d/mongodb stop"
+    alias mongo="/opt/mongodb/bin/mongo"
 else
   if [ ${OSTYPE:0:6} = "darwin" ]; then
     alias mongo-up="mongod run --logpath=/usr/local/var/log/mongodb/mongodb.log --logappend --fork --config /usr/local/Cellar/mongodb/2.0.1-x86_64/mongod.conf"
