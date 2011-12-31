124d270f007d82f12fcaabe069c2fb672070ae6d
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-12-31 14:34:33 +0100

    feed my inner child with gimme()

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,7 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+if [[ $HAS_BREW -eq 1 ]]; then
+  compctl -K _gimme gimme
+fi
