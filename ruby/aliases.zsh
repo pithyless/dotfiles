9e21a8d111217bd236822758e29f43691b1e2a00
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2012-01-23 11:34:42 +0100

    add pad-start alias for linux

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,7 +1,11 @@
+alias rvm-global='rvm use 1.9.2@global'
+
 alias r='rails'
 alias rc='rails console'
 
 alias p='padrino'
 alias pc='padrino console'
 
-alias rvm-global='rvm use 1.9.2@global'
+if [[ $IS_LINUX -eq 1 ]]; then
+  alias pad-start="echo Rock \'n Roll!; bundle exec padrino start -h 0.0.0.0"
+fi
