525bf2f376c2cae5e362e40613bdb2b64c47c818
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2013-03-17 10:17:54 +0100

    Add PYTHONPATH

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,5 +1,7 @@
-export EDITOR='mvim'
+export EDITOR='vim'
 
 export CLASSPATH=$CLASSPATH:/usr/local/Cellar/clojure-contrib/1.2.0/clojure-contrib.jar
 
 export PATH=/usr/local/share/npm/bin:${PATH}
+
+export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
